55b6d016599d4dbcec8c41bd642e149be6032c92
James Parrish <jthomasparrishjr@gmail.com>
2023-02-06 20:46:15 -0600

    commit tree 71181731e3ffa59874df9593fa14cb43b2c9802d

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,7 @@
 class BirdsController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response 
+  rescue_from ActiveRecord::RecordInvalid, with: 
+  :render_unprocessable_entity_response 
 
   # GET /birds
   def index
@@ -9,9 +11,9 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
-  end
+end
 
   # GET /birds/:id
   def show
@@ -22,8 +24,8 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
-    render json: bird
+    bird.update!(bird_params)
+    render json: bird 
   end
 
   # DELETE /birds/:id
@@ -45,6 +47,10 @@ class BirdsController < ApplicationController
 
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
+  end 
+
+  def render_unprocessable_entity_response(invalid) 
+    renders json: {errors: invalid.record.errors}, status: :unprocessable_entity 
   end
 
 end
